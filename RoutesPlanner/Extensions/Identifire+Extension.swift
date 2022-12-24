556ad907c69654f2bafa3015faaa0b2632e8edb8
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2022-12-24 13:28:36 +0100

    add identifire for cells

---
+++ RoutesPlanner/Extensions/Identifire+Extension.swift
@@ -0,0 +1,6 @@
+extension String {
+    static let identifireRoutesTableCell = "RoutesTableViewCell"
+    static let identifireNewRouteCell = "NewRouteTableViewCell"
+    static let identifireSearchResultTableViewControllerCell = "SearchResultTableViewControllerCell"
+    static let identifireLocationsTableViewCell = "LocationsTableViewCell"
+}
