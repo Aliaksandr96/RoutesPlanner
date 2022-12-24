628c04abc2c46626337cda7dac80672bbf59d35a
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2022-12-24 13:42:24 +0100

    main router

---
+++ RoutesPlanner/Modules/Main/MainRouter.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+protocol MainRouterProtocol {
+    func openNewRouteModule(completion: @escaping (([Location]) -> Void))
+    func openRoutesView(completion: @escaping ((Route) -> Void))
+}
+
+final class MainRouter: MainRouterProtocol {
+    let navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        let view = MainViewController()
+        let presenter = MainPresenter(view: view, router: self)
+        view.presenter = presenter
+        navigationController.pushViewController(view, animated: true)
+    }
+
+    func openNewRouteModule(completion: @escaping (([Location]) -> Void)) {
+        _ = NewRouteRouter(navigationController: navigationController, completion: completion)
+    }
+
+    func openRoutesView(completion: @escaping ((Route) -> Void)) {
+        _ = RoutesRouter(navigationController: navigationController, completion: completion)
+    }
+}
