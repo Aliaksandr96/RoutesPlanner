dd8a5e5fa350bf38a9f8406f970581b33b53f34d
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2022-12-24 13:49:25 +0100

    new route presenter

---
+++ RoutesPlanner/Modules/NewRoute/NewRoutePresenter.swift
@@ -0,0 +1,65 @@
+import Foundation
+import MapKit
+
+protocol NewRoutePresenterProtocol {
+    var locationsArray: [Location] { get set }
+    func updateTable()
+    func saveRouteToDatabase()
+    func checkRouteAndSave()
+    func popToRoot()
+    func transferCurrentLocationsArrayToMainTable(locations: [Location])
+}
+
+final class NewRoutePresenter {
+    // MARK: - Public
+
+    unowned var view: NewRouteViewProtocol
+
+    // MARK: - Private
+
+    private let router: NewRouteRouterProtocol
+
+    // MARK: - Variables
+
+    var locationsArray: [Location] = []
+    var transferLocationsArray: (([Location]) -> Void)?
+
+    init(view: NewRouteViewProtocol, router: NewRouteRouterProtocol) {
+        self.view = view
+        self.router = router
+    }
+}
+
+// MARK: - Extension
+
+extension NewRoutePresenter: NewRoutePresenterProtocol {
+    func checkRouteAndSave() {
+        if locationsArray.isEmpty == true {
+            view.showEmptyLocationsArrayAlert()
+        } else {
+            saveRouteToDatabase()
+            transferCurrentLocationsArrayToMainTable(locations: locationsArray)
+            popToRoot()
+        }
+    }
+
+    func updateTable() {
+        view.updateNewRouteTableView()
+    }
+
+    func saveRouteToDatabase() {
+        let route = Route()
+        route.dateSaving = Date.formating(date: Date())
+        route.nameRoute = Date.routeDay()
+        route.location.append(objectsIn: locationsArray)
+        DatabaseManager.shared.saveRoute(route: route)
+    }
+
+    func popToRoot() {
+        router.popToRoot()
+    }
+
+    func transferCurrentLocationsArrayToMainTable(locations: [Location]) {
+        transferLocationsArray?(locationsArray)
+    }
+}
