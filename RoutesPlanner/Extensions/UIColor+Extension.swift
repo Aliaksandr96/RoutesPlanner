54b73f16480be78cd421bad96d4d7cdac24241cd
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2022-12-24 13:29:08 +0100

    create custom colors

---
+++ RoutesPlanner/Extensions/UIColor+Extension.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+extension UIColor {
+    static func backgroundColor() -> UIColor {
+        UIColor(named: "backgroundColor")!
+    }
+
+    static func blackGold() -> UIColor {
+        UIColor(named: "black-gold")!
+    }
+
+    static func customBlue() -> UIColor {
+        UIColor(named: "customBlue")!
+    }
+
+    static func grayBlue() -> UIColor {
+        UIColor(named: "gray-blue")!
+    }
+
+    static func grayWhite() -> UIColor {
+        UIColor(named: "gray-white")!
+    }
+
+    static func pinBlue() -> UIColor {
+        UIColor(named: "pinBlue")!
+    }
+
+    static func pinRed() -> UIColor {
+        UIColor(named: "pinRed")!
+    }
+
+    static func pinGreen() -> UIColor {
+        UIColor(named: "pinGreen")!
+    }
+
+    static func blueGold() -> UIColor {
+        UIColor(named: "blue-gold")!
+    }
+}
