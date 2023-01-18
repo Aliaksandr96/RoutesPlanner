085558c014dcb87aa8c91eedc72c38590b93e43e
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2023-01-18 21:15:19 +0100

    add customer model

---
+++ RoutesPlanner/Models/ClientInfo.swift
@@ -0,0 +1,8 @@
+import RealmSwift
+import Foundation
+
+class ClientInfo: Object {
+    @objc dynamic var apartmentNumber: String = ""
+    @objc dynamic var clientName: String = ""
+    @objc dynamic var telNumber: String = ""
+}
