baa2117aade2dc0c47a23d57b385573b648116f9
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2023-01-19 19:48:19 +0100

    delete model

--- RoutesPlanner/Models/ClientInfo.swift
+++
@@ -1,8 +0,0 @@
-import RealmSwift
-import Foundation
-
-class ClientInfo: Object {
-    @objc dynamic var apartmentNumber: String = ""
-    @objc dynamic var clientName: String = ""
-    @objc dynamic var telNumber: String = ""
-}
