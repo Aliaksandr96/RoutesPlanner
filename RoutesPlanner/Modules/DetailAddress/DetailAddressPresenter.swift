29112d0ceb6ff891158634c90a10de2644e8d714
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2023-01-22 19:10:10 +0100

    save cusstomer detail info

--- RoutesPlanner/Modules/DetailAddress/DetailAddressPresenter.swift
+++ RoutesPlanner/Modules/DetailAddress/DetailAddressPresenter.swift
@@ -2,34 +2,46 @@ import Foundation
 
 protocol DetailAddressPresenterProtocol {
     func dissmisView()
-    func getLocation()
+    func updateLabelsFromGettingLocation()
     func updatePlaceholders()
     func updateCustomerInfo(name: String, flat: String, tel: String)
+    func saveInfoCustomerFromTextFields()
 }
 
 final class DetailAddressPresenter {
     // MARK: - Public
+
     unowned var view: DetailAddressViewProtocol
-    
+
     // MARK: - Private
+
     private let router: DetailAddressRouterProtocol
     private let location: Location
+
+    // MARK: - Variables
+
+    var transferCustomerInfo: ((Location) -> Void)?
 
     init(view: DetailAddressViewProtocol, router: DetailAddressRouterProtocol, location: Location) {
         self.view = view
         self.router = router
         self.location = location
-        self.getLocation()
-        self.updatePlaceholders()
+        updateLabelsFromGettingLocation()
+        updatePlaceholders()
     }
 }
 
 // MARK: - Extension
+
 extension DetailAddressPresenter: DetailAddressPresenterProtocol {
+    func saveInfoCustomerFromTextFields() {
+        transferCustomerInfo?(location)
+    }
+
     func updatePlaceholders() {
         view.setupPlaceholders(name: location.nameCustomer, flat: location.flatCustomer, tel: location.telCustomer)
     }
-    
+
     func updateCustomerInfo(name: String, flat: String, tel: String) {
         if !name.isEmpty {
             DatabaseManager.shared.customerName(name: name, location: location)
@@ -41,12 +53,13 @@ extension DetailAddressPresenter: DetailAddressPresenterProtocol {
         if !tel.isEmpty {
             DatabaseManager.shared.customerTel(tel: tel, location: location)
         }
-  
     }
+
     func dissmisView() {
         router.dissmisView()
     }
-    func getLocation() {
+
+    func updateLabelsFromGettingLocation() {
         view.updateLabels(buildNumber: location.building,
                           street: location.street,
                           postCode: location.postCode,
