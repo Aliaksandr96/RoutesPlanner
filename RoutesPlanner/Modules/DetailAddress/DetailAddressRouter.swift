bab3fa739f91d518f62364b1e25fcc1de70e5446
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2023-01-22 19:11:48 +0100

    set and push location methods

--- RoutesPlanner/Modules/DetailAddress/DetailAddressRouter.swift
+++ RoutesPlanner/Modules/DetailAddress/DetailAddressRouter.swift
@@ -7,13 +7,18 @@ protocol DetailAddressRouterProtocol {
 final class DetailAddressRouter: DetailAddressRouterProtocol {
     let navigationController: UINavigationController
     let location: Location
+    let completion: (Location) -> Void
     
-    init(navigationController: UINavigationController, location: Location) {
+    init(navigationController: UINavigationController, location: Location, completion: @escaping (Location) -> Void) {
         self.navigationController = navigationController
         self.location = location
+        self.completion = completion
         let view = DetailAddressViewController()
         let presenter = DetailAddressPresenter(view: view, router: self, location: location)
         view.presenter = presenter
+        presenter.transferCustomerInfo = { [weak self] locations in
+            self?.completion(locations)
+        }
         navigationController.present(view, animated: true)
     }
     func dissmisView() {
